e8077478eec8eb9ab6f41fef4fd2564baae1e03a
cinchronicity <168139730+cinchronicity@users.noreply.github.com>
2024-11-05 00:15:39 -0800

    attempt to fix overflowing imgs

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import './movie-card.css';
 
-export const MovieCard = ({ movie, onMovieClick, onAddToFavorites }) => {
+//add onAddToFavorites prop to MovieCard component later
+export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div onClick={() => onMovieClick(movie)}
-    >
-      {movie.title}
-    </div>
+
+    <Card className= "movie-card-container" onClick={() => onMovieClick(movie)}>
+      <Card.Img variant="top" src={movie.imageURL} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.description}</Card.Text>
+        <Button variant="link">Open</Button>
+      </Card.Body>
+    </Card>
   );
-}
-//define all the prop types that the component will use 
-//this will help catch bugs and make sure component recieves the correct data structure 
+};
+//define all the prop types that the component will use
+//this will help catch bugs and make sure component recieves the correct data structure
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
@@ -30,4 +38,4 @@ MovieCard.propTypes = {
     actors: PropTypes.array,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
-}; 
+};
